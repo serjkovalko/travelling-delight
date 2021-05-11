6ba20a319e23b1d44e1ee03a94cad6c6397ed363
serjkovalko <serjkovalko@gmail.com>
2021-05-11 13:08:10 +0300

    finished project

--- app/js/main.js
+++ app/js/main.js
@@ -14,4 +14,47 @@ $(function () {
   wow.init();
 });
 
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    const clock = document.querySelector('.content__timer');
+    const daysSpan = clock.querySelector('.content__timer-days');
+    const hoursSpan = clock.querySelector('.content__timer-hours');
+    const minutesSpan = clock.querySelector('.content__timer-minutes');
+    const secondsSpan = clock.querySelector('.content__timer-seconds');
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  const deadline = $('.content__timer').attr('data-time');
+  initializeClock('content__timer', deadline);
+
 })
